27fcc6590594cb1da9b486b5148c89f78b51145f
sam <samsam2310@gmail.com>
2017-06-28 19:33:18 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# try:
+#     from setuptools import setup
+# except ImportError:
+#     from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mealtime',
+    version='0.1.0b1',
+    author='Sam Wu',
+    author_email='samsam2310@gmail.com',
+    packages=find_packages(),
+    install_requires=['pymongo',
+                      'tornado',
+                      'requests'],
+)
