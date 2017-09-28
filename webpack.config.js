ac731442f72cf73801e91be0af74ee4e43dd7e07
samsam2310 <samsam2310@gmail.com>
2017-09-28 18:37:28 +0800

    clear code

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// 檔案起始點從 entry 進入，也可以是多個檔案。output 是放入產生出來的結果的相關參數。loaders 則是放欲使用的 loaders，在這邊是使用 babel-loader 將所有 .js（這邊用到正則式）相關檔案轉譯成瀏覽器可以閱讀的 JavaScript。devServer 則是 webpack-dev-server 設定。plugins 放置所使用的外掛
+
 module.exports = {
   node: {
   	__dirname: true
