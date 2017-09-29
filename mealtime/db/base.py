3fb6d9715be5b316d621d34cf61243bdd8d2460f
sam <samsam2310@gmail.com>
2017-09-30 03:35:28 +0900

    update orm base

--- mealtime/db/base.py
+++ mealtime/db/base.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-
 # DB - Pymongo ORM Base Class
 
 import os
 import pymongo
 
-DB_HOST		= os.environ.get('DB_HOST', 'localhost:27017').split(",")
-DB_REPLSET	= os.environ.get('DB_REPLSET', None)
-DB_USER		= os.environ.get('DB_USER', '')
-DB_PWD		= os.environ.get('DB_PWD', '')
-DB_NAME		= os.environ.get('DB_NAME', 'mealtime')
+DB_HOST = os.environ.get('DB_HOST', 'localhost:27017').split(",")
+DB_REPLSET = os.environ.get('DB_REPLSET', None)
+DB_USER = os.environ.get('DB_USER', '')
+DB_PWD = os.environ.get('DB_PWD', '')
+DB_NAME = os.environ.get('DB_NAME', 'mealtime')
 # The time out of MongoClient, in seconds.
 DB_TIMEOUT = 2
 
-def getDatabaseFromEnv(is_log_mode = False):
+
+def getDatabaseFromEnv(is_log_mode=False):
     if is_log_mode:
         print("Connect to DB: %s/%s" % (','.join(DB_HOST), DB_NAME))
         if DB_REPLSET:
@@ -26,9 +26,10 @@ def getDatabaseFromEnv(is_log_mode = False):
         print('Missing Replica set.')
         return None
 
-    mongo_client =
-    	MongoClient(host=DB_HOST, replicaset=DB_REPLSET,
-    		serverSelectionTimeoutMS=DB_TIMEOUT)
+    mongo_client = MongoClient(
+        host=DB_HOST,
+        replicaset=DB_REPLSET,
+        serverSelectionTimeoutMS=DB_TIMEOUT)
     database = mongo_client[DB_NAME]
 
     if DB_USER != '':
@@ -37,71 +38,87 @@ def getDatabaseFromEnv(is_log_mode = False):
             return None
     return database
 
+
 class Field():
-	def __init__(self, default = None):
-		self._default = default
+    def __init__(self, default=None):
+        self._default = default
 
-	def getDefault():
-		default = self._default
-		return default() if callable(default) else default
+    def getDefault():
+        default = self._default
+        return default() if callable(default) else default
 
-class Table():
-	_ORM_field_names = None
-	_ORM_mongo_client = getDatabaseFromEnv(True)
 
-	@classmethod
-	def _ormFieldInit(cls):
-		_ORM_field_names =
-			[attr for attr in cls.__dict__ if isinstance(attr, Field)]
+class Collection():
+    _ORM_field_names = None
+    _ORM_database = getDatabaseFromEnv(True)
+    # Overrite by subclass
+    _ORM_name = 'default'
 
-	@classmethod
-	def _checkInstance(cls, val, message):
-		if not isinstance(orm_object, cls):
-			raise ValueError(message)
+    @classmethod
+    def _ormFieldInit(cls):
+        _ORM_field_names = [
+            attr for attr in cls.__dict__ if isinstance(attr, Field)
+        ]
 
-	@classmethod
-	def insert(cls, orm_object):
-		cls._checkInstance('orm_object must be a isinstance of %s' % cls)
-		# TODO
+    @classmethod
+    def _checkInstance(cls, val, message):
+        if not isinstance(orm_object, cls):
+            raise ValueError(message)
 
-	@classmethod
-	def upsert(cls, orm_object, query_keys):
-		cls._checkInstance('orm_object must be a isinstance of %s' % cls)
-		# TODO
+    @classmethod
+    def insert(cls, orm_object):
+        cls._checkInstance('orm_object must be a isinstance of %s' % cls)
+        # TODO
 
-	@classmethod
-	def getCollectionName(cls):
-		return cls._ORM_collection_name
+    @classmethod
+    def upsert(cls, orm_object, query):
+        cls._checkInstance('orm_object must be a isinstance of %s' % cls)
+        # TODO
 
-	def __init__(self, *args, **kargs):
-		if not self.__class__._ORM_field_names:
-			self.__class__._ormFieldInit()
+    @classmethod
+    def update(cls, orm_object, query):
+        cls._checkInstance('orm_object must be a isinstance of %s' % cls)
+        # TODO
 
-		self._data = {
-			'_id': kargs.get('_id', None)
-		}
-		for attr in self.__class__._ORM_field_names:
-			self._data[attr] = kargs.get(attr, )
-			if attr in kargs:
-				self._data[attr] = kargs[attr]
-			else:
-				field = getattr(self.__class__, attr)
-				self._data[attr] = field.getDefault()
+    @classmethod
+    def query(cls, **kargs):
+        pass
+        # TODO
 
-	def __getitem__(self, key):
-		return self._data[key]
+    @classmethod
+    def getCollection(cls):
+        return cls._ORM_database[cls._ORM_name]
 
-	def __setitem(self, key, val):
-		self._data[key] = val
+    def __init__(self, *args, **kargs):
+        if not self.__class__._ORM_field_names:
+            self.__class__._ormFieldInit()
 
-	def saveORM(self):
-		if not self._id:
-			# TODO : add message
-			raise RuntimeError('')
-		# TODO
+        self._data = {'_id': kargs.get('_id', None)}
+        for attr in self.__class__._ORM_field_names:
+            self._data[attr] = kargs.get(attr, )
+            if attr in kargs:
+                self._data[attr] = kargs[attr]
+            else:
+                field = getattr(self.__class__, attr)
+                self._data[attr] = field.getDefault()
 
-	def deleteORM(self):
-		if not self._id:
-			# TODO : add message
-			raise RuntimeError('')
-		# TODO
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __setitem__(self, key, val):
+        self._data[key] = val
+
+    def save(self):
+        if not self._id:
+            # TODO : add message
+            raise RuntimeError('')
+        # TODO : maybe need data filter to remove _id in $set
+        self.getCollection().update_one({
+            '_id': self._data['_id']
+        }, {'$set': self._data})
+
+    def delete(self):
+        if not self._id:
+            # TODO : add message
+            raise RuntimeError('')
+        # TODO
